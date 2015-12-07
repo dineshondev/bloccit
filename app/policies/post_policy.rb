59c71a2eb4aeb034c821b27a120c868431b50457
dinesh ramadoss <dineshr@alumni.cmu.edu>
2015-12-07 03:10:48 -0500

    Access restriction for member

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,3 +1,20 @@
 class PostPolicy < ApplicationPolicy
+  
+  class Scope < Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where("user_id == ?", user.id)
+      end
+    end
+  end
 
 end
