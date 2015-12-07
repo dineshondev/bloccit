8ba3c8432d2d91a93d02d2ab036088d74c8a7b93
dinesh ramadoss <dineshr@alumni.cmu.edu>
2015-12-07 04:13:30 -0500

    adding posts under topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,16 @@ require 'faker'
    user.save!
  end
  users = User.all
- 
+
+ # Create Topics
+ 15.times do
+  Topic.create!(
+    name:         Faker::Lorem.sentence,
+    description:  Faker::Lorem.paragraph
+  )
+ end
+ topics = Topic.all
+
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
  
@@ -24,8 +33,9 @@ require 'faker'
 50.times do
   Post.create!(
     user:   users.sample,
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
+    topic:  topics.sample,
+    title:  Faker::Lorem.sentence,
+    body:   Faker::Lorem.paragraph
   )
 end
 posts = Post.all
