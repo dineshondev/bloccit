b68f27e0216a49db9bc862b8b071835829a888bd
dinesh ramadoss <dineshr@alumni.cmu.edu>
2016-04-05 04:00:12 -0400

    mocking rspec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,18 +1,30 @@
 require 'rails_helper'
 
 describe Vote do
+
+  include TestFactories
+
    describe "validations" do
      describe "valid validation" do
        it "only allows -1 or 1 as values" do
-         @post = Post.create(title: 'post title', body: 'post body must be pretty long with minimum 20 characters.')
-         3.times { @post.votes.create(value: 1) }
-         2.times { @post.votes.create(value: -1) }
-       
-         @post.votes.each do |vote|
-           expect(vote.value).to eq(1).or(eq(-1))
-         end
+         up_vote = Vote.new(value: 1)
+         expect(up_vote.valid?).to eq(true)
          
+         down_vote = Vote.new(value: -1)
+         expect(down_vote.valid?).to eq(true)
+
+         invalid_vote = Vote.new(value: 2)
+         expect(invalid_vote.valid?).to eq(false)
        end
      end
    end
- end 
+
+   describe 'after_save' do
+    it "calls 'Post#update_rank' after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
+    end
+  end
+end 
