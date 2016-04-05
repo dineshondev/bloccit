e89ffbd62e0f65740f9d74137aadd8444bf8dfad
dinesh ramadoss <dineshr@alumni.cmu.edu>
2016-04-05 07:05:09 -0400

    private topics

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @comments = @post.comments
     @comment = @post.comments.build
-    ##authorize @comment
+    authorize @topic
   end
 
   def new
